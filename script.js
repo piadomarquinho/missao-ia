ec1f88438af640ebc60e504102752711ea12af0a
Vini Dercacz <140642398+piadomarquinho@users.noreply.github.com>
2025-10-22 14:24:46 -0300

    rejk kfjdbspiç

--- script.js
+++ script.js
@@ -12,5 +12,33 @@ let perguntaAtual;
 let historiaFinal = "";
 
 botaoIniciar.addEventListener('click,iniciaJogo');
+function iniciaJogo (){
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none' ;
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostrapergunta();
+}
 
+function mostrapergunta (){
+    if (atual >= Perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = pergunta[atual];
+    caixaPerguntas.textcontent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
 
+    function mostraAlternativas(){
+    for (const alternativas of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+
+    }
