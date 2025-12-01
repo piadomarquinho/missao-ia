c455807ad5f565aed4c798b196a5682ce3852136
Vini Dercacz <140642398+piadomarquinho@users.noreply.github.com>
2025-12-01 09:30:46 -0300

    ddklf

---
+++ aleatorio.js
@@ -0,0 +1,6 @@
+const nomes = ["Karine", "Felipe", "Augusto", "Maria", "Lisiane"];
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()*lista.lenght);
+    return lista[posicao];
+}
+export const nome = aleatorio(nomes)
